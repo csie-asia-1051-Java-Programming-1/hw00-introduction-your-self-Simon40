b079af28fef0fcf60e346ec54e0686f9c2874a08
Simon40 <105021060@live.asia.edu.tw>
2016-09-12 15:48:47 +0800

    hw-00-simon40 homework submit
    開學第一天

---
+++ hw-00-simon40/src/Simon1.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Simon1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello!!");
+        System.out.println("My name is Simon");
+        System.out.println("I'm a student in Aisa University from Taiwan.");
+	}
+
+}
