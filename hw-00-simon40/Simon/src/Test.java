ebbf6bb45b32abf750e1877bbb172a0d0febccfd
Simon40 <105021060@live.asia.edu.tw>
2016-09-19 21:44:25 +0800

    simulation practice

---
+++ hw-00-simon40/Simon/src/Test.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Please input a=");
+        int a=scn.nextInt();
+        System.out.println("Please input b=");
+        int b=scn.nextInt();
+        int c=a+b;
+        int d=a-b;
+        int e=a*b;
+        int f=(int)a/b;
+        float g=(float)a%b;
+        System.out.println("a+b="+c);
+        System.out.println("a-b="+d);
+        System.out.println("a*b="+e);
+        System.out.println("a/b="+f);
+        System.out.println("a%b="+g);
+	}
+
+}
---
+++ hw-00-simon40/Test1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn =new Scanner(System.in);
+        System.out.println("Please input mile=");
+        int m=scn.nextInt();
+        double k=(double)m*1.6;
+        System.out.println("km="+k);
+	}
+
+}
